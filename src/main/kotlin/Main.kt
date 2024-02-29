9e914e54212aefe76849dfa29c23e3451be87852
hitendraquessou <sanjay_quessou@yahoo.co.uk>
2024-02-29 15:45:08 +0000

    feat: Implemented Generics in Kotlin

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,3 +1,14 @@
 fun main(args: Array<String>) {
-   
+
+    val question1 = Question<String>("Quoth the raven ___", "nevermore", "medium")
+    val question2 = Question<Boolean>("The Sky is green. True or false", false, "easy")
+    val question3 = Question<Int>("How many days are there between full moons?", 28, "hard")
+    println(question1.questionText)
+}
+
+class Question<T>(
+    val questionText: String,
+    val answer: T,
+    val difficulty: String
+) {
 }
